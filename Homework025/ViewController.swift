481eb471f95da9935e062016ba146c8d2e77fd2c
Poga Huang <pgh86812@gmail.com>
2021-11-10 14:51:44 +0800

    commit 1

--- Homework025/ViewController.swift
+++ Homework025/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var dateAndTimeLabel: UILabel!
+    @IBOutlet weak var myDatePicker: UIDatePicker!
+    @IBAction func showDateAndTime(_ sender: UIDatePicker) {
+        
+        let selectTime = myDatePicker.date
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        dateAndTimeLabel.text = formatter.string(from: selectTime)
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
